b067e382eb0a686b96a392666d0444c86b863a9e
Isaac Allport <48483898+Caasitroplla@users.noreply.github.com>
2021-07-07 21:18:06 +0100

    Some starter code

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 #This will be where scripts will be imported and run
 
+'''
+Step 1 - Divide the decimal number to be converted by the value of the new base
+Step 2 - Get the remainder from Step 1 as the rightmost digit (least significant digit) of new base number.
+Step 3 - Divide the quotient of the previous divide by the new base
+Step 4 - Record the remainder from Step 3 as the next digit (to the left) of the new base number
+'''
+
+# hate this arracy would remove it if not necessary
+allChars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
 def main():
   pass
+
+def normalise(input, inputBase):
+
+  characters = []
+
+  for char in input:
+    characters.append(char) # replace with search function appending place vlaue to array
+
+
+
